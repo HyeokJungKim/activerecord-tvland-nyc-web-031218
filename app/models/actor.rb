50f74e0f0c916f0108803dd6a2f417ee14efb08e
Eric Kim <hyeokjungkim@gmail.com>
2018-03-23 14:06:55 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,24 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+
+    arr = self.characters.map do |character|
+      character.name
+    end
+
+    i = 0
+    self.characters.each do |character|
+        arr[i] += " - " + character.show.name
+        i += 1
+    end
+    return arr
+  end
+
 end
